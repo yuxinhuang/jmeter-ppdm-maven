9953a6d53cba38bb92e72129016bf09b64bef4c3
Kevin Kodama <Kevin.Kodama@Dell.com>
2022-08-15 12:02:06 -0700

    Log POST bodies

--- run.sh
+++ run.sh
@@ -2,26 +2,23 @@
 # arg[1] csv file directory
 RESULT_DIR=target/jmeter/results
 mvn clean
-if [ -d "listener-data" ]; then
-	rm -r listener-data/*
-else
-	mkdir listener-data
-fi
 {
 	read
 	while IFS=',', read -r testNum IP port URI username password verb bodyFile numDependencies dependencies numThreads requestsPerThread rampUpTime || [ -n "$rampUpTime" ]
 	do
 		echo "Test Number : $testNum"
-		TEST_DIR=$RESULT_DIR/test-$testNum
-		POST_DIR=listener-data/failed-post-requests
-		if [ $verb == "POST" ]; then
-			mkdir $POST_DIR
-		fi
-
 		mvn install -DtestNum=$testNum -DIP=$IP -Dport=$port -DURI=${URI//////} -Dusername=$username -Dpassword=$password -Dverb=$verb -DbodyFile=$bodyFile -DnumDependencies=$numDependencies -Ddependencies=$dependencies -DnumThreads=$numThreads -DrequestsPerThread=$requestsPerThread -DrampUpTime=$rampUpTime -Dreportgenerator.properties.overall_granularity=1000 &
 		wait
 
+		TEST_DIR=$RESULT_DIR/test-$testNum
 		mkdir $TEST_DIR
+		mkdir target/jmeter/listener-data
+		POST_DIR=target/jmeter/listener-data/failed-post-requests
+		if [ $verb == "POST" ]; then
+			mkdir $POST_DIR
+			cp -vr target/????????-????-????-????-????????????/jmeter/bin/data/*.txt $POST_DIR
+		fi
+		rm -vr target/????????-????-????-????-????????????
 		mv $RESULT_DIR/*.csv $RESULT_DIR/test-$testNum-jtl.csv
 		cp $RESULT_DIR/*.csv $TEST_DIR
 		rm $RESULT_DIR/*.csv
@@ -34,11 +31,6 @@ fi
 	done
 } < src/test/jmeter/data/$1
 
-if [ $verb == "POST" ]; then
-	cp -r listener-data/failed-post-requests target/jmeter/results
-fi
-rm target/jmeter/results/*.csv
 rm -r target/jmeter/logs
 rm -r target/jmeter/reports
 rm -r target/jmeter/testFiles
-rm -vr target/????????-????-????-????-????????????
